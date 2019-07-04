a07eac8d7fe857156784c7af99668e1aba9d606c
Syun1Kito <syun19991119@gmail.com>
2019-07-04 16:29:53 +0900

    スクリーンショット機能

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;
+    Vector3 moveDirection;
+
+    public float gravity;
+    public float speed;
+    public float amountRotate;
+    public float speedJump;
+
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        moveDirection = Vector3.zero;
+    }
+
+    void Update()
+    {
+      
+            //Inputの検知->0未満となってバックすることを避ける
+            if (Input.GetAxis("Vertical") > 0.0f)
+            {
+                moveDirection.z = Input.GetAxis("Vertical") * speed;
+            }
+            else
+            {
+                moveDirection.z = 0;
+            }
+
+            //方向転換
+            transform.Rotate(0, Input.GetAxis("Horizontal") * amountRotate, 0);
+        //地上にいる場合のみ操作を行う
+        if (controller.isGrounded)
+        {
+            //ジャンプ
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = speedJump;
+            }
+        }
+        //毎フレーム重力を加算
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        //移動の実行
+        Vector3 globalDirection = transform.TransformDirection(moveDirection);
+        controller.Move(globalDirection * Time.deltaTime);
+
+        //移動後地面についてるなら重力をなくす
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0;
+        }
+    }
+}
