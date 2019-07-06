f854fdd590bf3936b2dcc80e4db62096624b3b0a
ikeguchi7777 <ikeguchi.0723@gmail.com>
2019-07-06 14:17:50 +0900

    RenderTextureで画像をPlaneに表示

---
+++ Assets/Scripts/Player/SmartPhoneCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartPhoneCamera : MonoBehaviour
+{
+    Camera _camera;
+    RenderTexture _panelTexture;
+    [SerializeField]
+    Material panelMaterial;
+    [SerializeField]
+    int width = 375, height = 812;
+    
+    void Awake()
+    {
+        _camera = GetComponentInChildren<Camera>();
+        var textureDescriptor = new RenderTextureDescriptor(width, height);
+        _panelTexture = new RenderTexture(textureDescriptor);
+        _camera.targetTexture = _panelTexture;
+        panelMaterial.mainTexture = _panelTexture;
+        panelMaterial.SetTexture("_EmissionMap", _panelTexture);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
