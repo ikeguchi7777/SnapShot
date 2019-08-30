110b3ee491cf44f2414f3f9205c7a9b270bd7fda
ikeguchi7777 <ikeguchi.0723@gmail.com>
2019-08-30 17:38:27 +0900

    Revert "コンフリクト修正"
    This reverts commit 047ad34a52b8ce41ea53b84d35ecb13a78dafac1.

--- Assets/Scripts/ScoreCalculator.cs
+++ Assets/Scripts/ScoreCalculator.cs
@@ -5,65 +5,60 @@ using UnityEngine;
 public class ScoreCalculator : MonoBehaviour
 {
 
-    //PlayerController playerController;
-    Camera camera;
-    //GameObject[] Enemy;
+    GameObject[] Enemy;
+    float distanceScore = 0;// { get; private set; }
+    [SerializeField] float distanceWeight = 10;
+    float angleScore = 0;// { get; private set; }
+    [SerializeField] float angleWeight = 100 / 180;
+    float positionScore = 0;// { get; private set; }
+    [SerializeField] float positionWeight = 1;
 
-    public float distanceScore { get; private set; } = 0;
-    float distanceWeight = 10;
-
-    public float angleScore { get; private set; } = 0;
-    float angleWeight = 100 / 180f;
-
-    public float positionScore { get; private set; } = 0;
-    float positionWeight = 1;
-
-
-
-    //playerController = GetComponent<PlayerController>();
-    //camera = playerController.GetCamera();
-    //Enemy = playerController.GetEnemy();
-
-
-    public ScoreCalculator(Camera camera)
-    {
-        this.camera = camera;
-    }
 
     public float CalcDistance(GameObject target)
     {
-        return Vector3.Distance(camera.transform.position, target.transform.position);
+        return Vector3.Distance(GetComponent<Camera>().transform.position, target.transform.position);
     }
 
     public float CalcAngle(GameObject target)
     {
-        return 180 - Vector3.Angle(camera.transform.forward, target.transform.forward);
+        return 180 - Vector3.Angle(GetComponent<Camera>().transform.forward, target.transform.forward);
     }
     public Vector2 CalcPosition(GameObject target)
     {
-        return (Vector2)camera.WorldToViewportPoint(target.transform.position);
+        return (Vector2)GetComponent<Camera>().WorldToViewportPoint(target.transform.position);
     }
 
+    /*public void Calc()
+    {
+
+        foreach (var ene in Enemy)
+        {
+            var targets = ene.GetComponent<PlayerController>().GetTarget();
+            foreach (var tar in targets)
+            {
+                distanceScore += distanceWeight * CalcDistance(tar);
+                angleScore += angleWeight * CalcAngle(tar);
+                positionScore += positionWeight * (1 - Mathf.Abs(2 * CalcPosition(tar).x - 1)) + positionWeight * (1 - Mathf.Abs(2 * CalcPosition(tar).x - 1));
+            }
+        }
+
+        Debug.Log("dis:"+distanceScore+" ang:"+angleScore+" pos:"+positionScore);
+    }*/
 
     public void Calc(GameObject tar)
     {
-
-
         float d = 100 - distanceWeight * Mathf.Abs(CalcDistance(tar) - 1);
-        d = d > 0 ? d : 0;
-
+            d = d > 0 ? d : 0;
         float a = angleWeight * CalcAngle(tar);
-        a = a > 0 ? a : 0;
-
-        float p = 100 - positionWeight * (50 * Mathf.Abs(2 * CalcPosition(tar).x - 1) + 50 * Mathf.Abs(2 * CalcPosition(tar).y - 1));
-        p = p > 0 ? p : 0;
+            a = a > 0 ? a : 0;
+        float p = 100 - positionWeight * (50 * Mathf.Abs(2 * CalcPosition(tar).x - 1) + 50 * Mathf.Abs(2 * CalcPosition(tar).x - 1));
+            p = p > 0 ? p : 0;
 
         distanceScore += d;
         angleScore += a;
         positionScore += p;
 
         Debug.Log("dis:" + d + " ang:" + a + " pos:" + p);
-
     }
 
     public void PrintScore()
@@ -71,5 +66,3 @@ public class ScoreCalculator : MonoBehaviour
         Debug.Log("dis:" + distanceScore + " ang:" + angleScore + " pos:" + positionScore);
     }
 }
-
-
