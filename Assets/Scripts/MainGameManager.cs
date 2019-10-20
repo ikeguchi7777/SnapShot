070c4113b82fe9426de42a7d5e4c871c5975df08
ikeguchi7777 <ikeguchi.0723@gmail.com>
2019-10-20 23:37:03 +0900

    ステージのコライダー設定完了

--- Assets/Scripts/MainGameManager.cs
+++ Assets/Scripts/MainGameManager.cs
@@ -7,6 +7,8 @@ public enum Item
 {
     Transparency,
     SpeedUp,
+    FindPlayer,
+    Warp,
     MaxNum
 }
 public class MainGameManager : GameManager
@@ -27,10 +29,36 @@ public class MainGameManager : GameManager
                 case Item.SpeedUp:
                     StartCoroutine(SpeedUpCoroutine(playerID));
                     break;
+                case Item.FindPlayer:
+                    StartCoroutine(FindPlayerCoroutine(playerID));
+                    break;
+                case Item.Warp:
+                    Warp(playerID);
+                    break;
                 default:
                     Debug.Log("効果が設定されていません");
                     break;
             }
+        }
+    }
+
+    void Warp(int playerID)
+    {
+        int id = 0;
+        int count = 0;
+        while (count != 10)
+        {
+            do
+            {
+                id = Random.Range(0, GameInstance.Instance.PlayerNum);
+            } while (id == playerID);
+            Vector3 result;
+            if (NavMeshSamplePosition.RandomPoint(Players[id].transform.position - Players[id].transform.forward * 10.0f, 5.0f, out result))
+            {
+                Players[playerID].transform.position = result;
+                return;
+            }
+            count++;
         }
     }
 
@@ -47,7 +75,12 @@ public class MainGameManager : GameManager
         {
             var t = duration[(int)Item.Transparency, playerID];
             duration[(int)Item.Transparency, playerID] = 0.0f;
-            yield return new WaitForSeconds(t);
+            while (t>0.0f)
+            {
+                if (!pause.isPaused)
+                    t -= Time.deltaTime;
+                yield return null;
+            }
         } while (duration[(int)Item.Transparency, playerID]>0);
         isenable[(int)Item.Transparency, playerID] = false;
         for (int i = 0; i < tpCameras.Length; i++)
@@ -66,11 +99,54 @@ public class MainGameManager : GameManager
         {
             var t = duration[(int)Item.SpeedUp, playerID];
             duration[(int)Item.SpeedUp, playerID] = 0.0f;
-            yield return new WaitForSeconds(t);
+            while (t > 0.0f)
+            {
+                if (!pause.isPaused)
+                    t -= Time.deltaTime;
+                yield return null;
+            }
         } while (duration[(int)Item.SpeedUp, playerID] > 0);
         Debug.Log("加速終了");
         isenable[(int)Item.SpeedUp, playerID] = false;
         Players[playerID].ChangeSpeed(0.5f);
+    }
+
+    IEnumerator FindPlayerCoroutine(int playerID)
+    {
+        yield return null;
+        do
+        {
+            var t = duration[(int)Item.FindPlayer, playerID];
+            var ids = new int[GameInstance.Instance.PlayerNum - 1];
+            int num = 0;
+            Vector3 view_pos;
+            for (int i = 0; i < GameInstance.Instance.PlayerNum; i++)
+            {
+                if (i == playerID)
+                    continue;
+                ids[num++] = i;
+            }
+            duration[(int)Item.FindPlayer, playerID] = 0.0f;
+            while (t > 0.0f)
+            {
+                if (pause.isPaused)
+                    yield return null;
+                t -= Time.deltaTime;
+                var cam = Players[playerID].currentCamera;
+                tpCameras[playerID].ClearPlayerIcon();
+                foreach (var id in ids)
+                {
+                    if (Players[id].IsInViewport(cam,out view_pos))
+                    {
+                        tpCameras[playerID].SetPlayerIcon(view_pos, id);
+                    }
+                }
+                yield return null;
+            }
+        } while (duration[(int)Item.FindPlayer, playerID] > 0);
+        Debug.Log("探索終了");
+        tpCameras[playerID].ClearPlayerIcon();
+        isenable[(int)Item.FindPlayer, playerID] = false;
     }
     protected override void Update()
     {
--- Assets/Scripts/NavMesh/NavMeshSamplePosition.cs
+++ Assets/Scripts/NavMesh/NavMeshSamplePosition.cs
@@ -27,6 +27,22 @@ public class NavMeshSamplePosition : MonoBehaviour
         return false;
     }
 
+    public static bool RandomPoint(Vector3 center,float radius, out Vector3 result)
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Vector3 randomPoint = center + Random.insideUnitSphere * radius;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+            {
+                result = hit.position;
+                return true;
+            }
+        }
+        result = Vector3.zero;
+        return false;
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, range);
