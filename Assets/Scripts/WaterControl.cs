db09472b1c53de2df4a751fa864c5d41996ddc91
ikeguchi7777 <37902302+ikeguchi7777@users.noreply.github.com>
2019-11-14 14:05:02 +0900

    水しぶきを追加

--- Assets/Scripts/WaterControl.cs
+++ Assets/Scripts/WaterControl.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class WaterControl : MonoBehaviour
 {
-    [SerializeField] RippleControl[] rippleControls;
+    [SerializeField] RippleControl[] rippleControls=null;
+    [SerializeField] GameObject splash=default;
     List<SnapShotPlayerController> players = new List<SnapShotPlayerController>();
+    [SerializeField] float splashRange = 5.0f;
     private void OnTriggerEnter(Collider other)
     {
         var player = other.gameObject.GetComponent<SnapShotPlayerController>();
@@ -15,6 +17,10 @@ public class WaterControl : MonoBehaviour
             rippleControls[player.PlayerID].enabled = true;
             rippleControls[player.PlayerID].parentObj = player.transform;
             player.ChangeWaterState(true);
+            if(Vector3.Dot(player._rigidbody.velocity,Vector3.down)>splashRange)
+            {
+                Instantiate(splash, new Vector3(player.transform.position.x, splash.transform.position.y, player.transform.position.z), splash.transform.rotation);
+            }
         }
         players.Add(player);
     }
@@ -28,6 +34,10 @@ public class WaterControl : MonoBehaviour
             Debug.Log("water out");
             rippleControls[player.PlayerID].enabled = false;
             player.ChangeWaterState(false);
+            if (Vector3.Dot(player._rigidbody.velocity, Vector3.up) > splashRange)
+            {
+                Instantiate(splash, new Vector3(player.transform.position.x, splash.transform.position.y, player.transform.position.z), splash.transform.rotation);
+            }
         }
     }
 }
