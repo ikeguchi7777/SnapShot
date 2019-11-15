038210d5dc4eff71e8c20f3608c5cc92eb675416
Syun1Kito <syun19991119@gmail.com>
2019-11-15 14:47:05 +0900

    ポーズ修正

--- Assets/Scripts/CountDown.cs
+++ Assets/Scripts/CountDown.cs
@@ -16,8 +16,8 @@ public class CountDown : MonoBehaviour
 
     [SerializeField]
     GameObject[] count;
-    float[] timepoint = {300,240,180,120, 60, 30, 10,  5,  4,  3,  2,  1,  0, -1};
-    //float[] timepoint = {300,295,290,285,280,275,270,269,268,267,265,264,263, -1};
+    float[] timepoint = {300,240,180,120, 60, 30, 10,  5,  4,  3,  2,  1,  0, -100};
+    //float[] timepoint = {300,295,290,285,280,275,270,269,268,267,265,264,263, -100};
   
     [SerializeField]
     int flag = 0;
@@ -60,7 +60,7 @@ public class CountDown : MonoBehaviour
                         Sequence seq = DOTween.Sequence();
                         seq.Append(count[i].GetComponent<Transform>().DOScale(new Vector2(0.5f, 0.5f), 0f));
                         seq.Append(count[i].GetComponent<CanvasGroup>().DOFade(1, 0.2f));
-                        seq.Join(count[i].GetComponent<Transform>().DOScale(new Vector2(1f, 1f), 0.2f));
+                        seq.Join(count[i].GetComponent<Transform>().DOScale(new Vector2(2f, 2f), 0.2f));
                         seq.AppendInterval(0.6f);
                         seq.Append(count[i].GetComponent<CanvasGroup>().DOFade(0, 0.2f));
                         seq.Join(count[i].GetComponent<Transform>().DOScale(new Vector2(0.1f, 0.1f), 0.2f));
